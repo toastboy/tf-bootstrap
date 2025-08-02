be4125933e82f5d94a47fb746cb834cf93fd9911
Jon Wilson <github@toastboy.co.uk>
2025-08-02 12:44:50 +0100

    SYS-429 Use account ID to create service token

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "toastboy"
+
+    workspaces {
+      name = "tf-bootstrap"
+    }
+  }
+
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
@@ -10,32 +18,14 @@ terraform {
       version = "~> 2.1.0"
     }
   }
-  cloud {
-    organization = "toastboy"
-
-    workspaces {
-      name = "tf-bootstrap"
-    }
-  }
 }
 
 provider "cloudflare" {
   api_token = var.bootstrap_cloudflare_api_token
 }
 
-data "cloudflare_zones" "zones" {
-  account = {
-    id = var.bootstrap_cloudflare_account_id
-  }
-  name = "toastboy.co.uk"
-}
-
-data "cloudflare_zone" "toastboy_co_uk" {
-  zone_id = data.cloudflare_zones.zones.result[0].id
-}
-
 resource "cloudflare_zero_trust_access_service_token" "onepassword_connect" {
-  name     = "1Password Connect"
-  zone_id  = data.cloudflare_zone.toastboy_co_uk.id
-  duration = "60m"
+  account_id = data.hcp_vault_secrets_app.cloudflare.secrets.accountid
+  name       = "1Password Connect"
+  duration   = "12h"
 }
