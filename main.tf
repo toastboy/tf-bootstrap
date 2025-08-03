85b87cd304d943a2f21de4c51eba660df09cddf8
Jon Wilson <github@toastboy.co.uk>
2025-08-03 21:00:28 +0100

    SYS-429 Add 1Password Connect Application and Access Policy

--- main.tf
+++ main.tf
@@ -29,3 +29,40 @@ resource "cloudflare_zero_trust_access_service_token" "onepassword_connect" {
   name       = "1Password Connect"
   duration   = "12h"
 }
+
+resource "cloudflare_zero_trust_access_policy" "onepassword_connect_service" {
+  account_id = var.bootstrap_cloudflare_account_id
+  name       = "1Password Connect Service Token Access"
+  decision   = "non_identity"
+
+  include = [
+    {
+      service_token = {
+        token_id = cloudflare_zero_trust_access_service_token.onepassword_connect.id
+      }
+    }
+  ]
+}
+
+data "cloudflare_zones" "toastboy_co_uk" {
+  account = {
+    id = var.bootstrap_cloudflare_account_id
+  }
+
+  name = "toastboy.co.uk"
+}
+
+resource "cloudflare_zero_trust_access_application" "service_application" {
+  zone_id = data.cloudflare_zones.toastboy_co_uk.result[0].id
+
+  name   = "onepassword-connect"
+  domain = "toastboy.co.uk"
+
+  type             = "self_hosted"
+  session_duration = "1h"
+
+  policies = [{
+    id         = cloudflare_zero_trust_access_policy.onepassword_connect_service.id
+    precedence = 1
+  }]
+}
